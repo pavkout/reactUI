ec45d10277030490dfc017de51b43b64150cc96d
Pavkout <pkoutoglou@gmail.com>
2016-11-28 22:38:49 +0200

    Add aliases.js
    Add aliases.js

---
+++ config/webpack/aliases.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+  //Components
+  button: 'src/components/button/button',
+
+
+  //Example
+  app: 'example/app',
+
+  // Provider
+  themeProvider: 'src/provider/themeProvider',
+  theme: 'src/containers/theme'
+  
+};
