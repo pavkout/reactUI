1bc5090fd977ce2f8ff21061bfa38b63c28d73a7
Pavkout <pkoutoglou@gmail.com>
2016-11-28 22:41:37 +0200

    Add theme component
    Add theme component

---
+++ src/containers/theme.jsx
@@ -0,0 +1,19 @@
+import React, { Component, PropTypes } from 'react';
+
+const theme = (ComponentToWrap) => {
+  return class ThemeComponent extends Component {
+    // let’s define what’s needed from the `context`
+    static contextTypes = {
+      theme: PropTypes.object.isRequired,
+    }
+    render() {
+      const { theme } = this.context;
+      // what we do is basically rendering `ComponentToWrap`
+      // with an added `theme` prop, like a hook
+      return (
+        <ComponentToWrap {...this.props} theme={theme} />
+      )
+    }
+  }
+}
+export default theme;
