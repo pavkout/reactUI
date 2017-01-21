0fc36c1d66d7aebfd59f527e736ca637ccce2214
Pavkout <pkoutoglou@gmail.com>
2017-01-21 03:12:16 +0200

    Add mising ;
    Add mising ;

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -12,25 +12,25 @@ const Button = styled.button`
   /*padding for all sizes*/
   ${props => props.size === 'xsmall' && 'padding: 1px 5px;'
     || props.size === 'small' && 'padding: 5px 10px;'
-    || props.size === 'medium' && 'padding: 6px 12px'
-    || props.size === 'large' && 'padding: 10px 16px'
-    || props.size === 'xlarge' && 'padding: 14px 20px'
+    || props.size === 'medium' && 'padding: 6px 12px;'
+    || props.size === 'large' && 'padding: 10px 16px;'
+    || props.size === 'xlarge' && 'padding: 14px 20px;'
   }
 
   /*border-radius for all sizes*/
   ${props => props.size === 'xsmall' && 'border-radius: 3px'
     || props.size === 'small' && 'border-radius: 3px;'
-    || props.size === 'medium' && 'border-radius: 4px'
-    || props.size === 'large' && 'border-radius: 6px'
-    || props.size === 'xlarge' && 'border-radius: 8px'
+    || props.size === 'medium' && 'border-radius: 4px;'
+    || props.size === 'large' && 'border-radius: 6px;'
+    || props.size === 'xlarge' && 'border-radius: 8px;'
   }
 
   /*font-size for all sizes*/
-  ${props => props.size === 'xsmall' && 'font-size: 12px'
+  ${props => props.size === 'xsmall' && 'font-size: 12px;'
     || props.size === 'small' && 'font-size: 12px;'
-    || props.size === 'medium' && 'font-size: 14px'
-    || props.size === 'large' && 'font-size: 18px'
-    || props.size === 'xlarge' && 'font-size: 18px'
+    || props.size === 'medium' && 'font-size: 14px;'
+    || props.size === 'large' && 'font-size: 18px;'
+    || props.size === 'xlarge' && 'font-size: 18px;'
   }
 `;
 
