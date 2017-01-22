d8ac1bb4b7e689ae7556b1b9d9eb3200529b148b
Pavkout <pkoutoglou@gmail.com>
2017-01-22 03:07:03 +0200

    Add functionalllity in button component
    Add functionalllity in button component

--- example/app.jsx
+++ example/app.jsx
@@ -5,67 +5,66 @@ import ThemeProvider from 'themeProvider';
 import Button from 'button';
 
 export default class App extends Component {
-  render() {
+  renderButtons(theme = 'light', size="xsmall") {
     return (
       <div>
-        <Grid style={{ backgroundColor: 'white', height: '150px', padding: '60px' }}>
-          <Row>
-            <Col sm={2}>
-              <ThemeProvider theme="dark" size="xsmall">
-                <Button>xsmall</Button>
-              </ThemeProvider>
-            </Col>
-            <Col sm={2}>
-              <ThemeProvider theme="dark" size="small">
-                <Button>small</Button>
-              </ThemeProvider>
-            </Col>
-            <Col sm={2}>
-              <ThemeProvider theme="dark" size="medium">
-                <Button>medium</Button>
-              </ThemeProvider>
-            </Col>
-            <Col sm={2}>
-              <ThemeProvider theme="dark" size="large">
-                <Button>large</Button>
-              </ThemeProvider>
-            </Col>
-            <Col sm={2}>
-              <ThemeProvider theme="dark" size="xlarge">
-                <Button>xlarge</Button>
-              </ThemeProvider>
-            </Col>
-          </Row>
-        </Grid>
-        <Grid style={{ backgroundColor: 'black', height: '150px', padding: '60px' }}>
-          <Row>
-            <Col sm={2}>
-              <ThemeProvider size="xsmall">
-                <Button>xsmall</Button>
-              </ThemeProvider>
-            </Col>
-            <Col sm={2}>
-              <ThemeProvider size="small">
-                <Button>small</Button>
-              </ThemeProvider>
-            </Col>
-            <Col sm={2}>
-              <ThemeProvider size="medium">
-                <Button>medium</Button>
-              </ThemeProvider>
-            </Col>
-            <Col sm={2}>
-              <ThemeProvider size="large">
-                <Button>large</Button>
-              </ThemeProvider>
-            </Col>
-            <Col sm={2}>
-              <ThemeProvider size="xlarge">
-                <Button>xlarge</Button>
-              </ThemeProvider>
-            </Col>
-          </Row>
-        </Grid>
+        <ThemeProvider theme={theme} size={size}>
+          <div>
+            <Button>Click me</Button>
+            <Button disabled>Click me</Button>
+            <Button display="link">Click me</Button>
+            <Button display="text">Click me</Button>
+            <Button withRoundedCorners>Click me</Button>
+            <Button withRoundedCorners fullWidth textPosition="left">Click me</Button>
+            <Button withRoundedCorners fullWidth textPosition="center">Click me</Button>
+            <Button withRoundedCorners fullWidth textPosition="right">Click me</Button>
+            <Button fullWidth>Click me</Button>
+            <Button withRoundedCorners icon="facebook" iconPosition="before">Click me</Button>
+            <Button withRoundedCorners icon="facebook" iconPosition="after">Click me</Button>
+            <Button withRoundedCorners icon="facebook" iconPosition="up">Click me</Button>
+            <Button withRoundedCorners icon="facebook" iconPosition="down">Click me</Button>
+            <Button withRoundedCorners icon="facebook"/>
+          </div>
+        </ThemeProvider>
+      </div>
+    );
+  }
+  render() {
+    return (
+      <div style={{ height: 'auto', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
+        <div style={{ width: '100%', display: 'flex', justifyContent: 'space-around', backgroundColor: 'cadetblue', color: 'white', height: 'auto', padding: '10px 70px', border: '1px solid black' }}>
+          <h3>xsmall size</h3>
+          <h3>small size</h3>
+          <h3>medium size</h3>
+          <h3>large size</h3>
+          <h3>xlarge size</h3>
+        </div>
+        <div style={{ width: '100%', display: 'flex', justifyContent: 'space-around', backgroundColor: 'oldlace', height: 'auto', padding: '60px', border: '1px solid black' }}>
+          {this.renderButtons("dark", 'xsmall')}
+          {this.renderButtons("dark", 'small')}
+          {this.renderButtons("dark", 'medium')}
+          {this.renderButtons("dark", 'large')}
+          {this.renderButtons("dark", 'xlarge')}
+        </div>
+        <div style={{ width: '100%', display: 'flex', justifyContent: 'space-around', backgroundColor: 'black', height: 'auto', padding: '60px', border: '1px solid black' }}>
+          {this.renderButtons("light", 'xsmall')}
+          {this.renderButtons("light", 'small')}
+          {this.renderButtons("light", 'medium')}
+          {this.renderButtons("light", 'large')}
+          {this.renderButtons("light", 'xlarge')}
+        </div>
+        <div style={{ width: '100%', border: '1px solid black',  backgroundColor: 'oldlace' }}>
+          <div style={{ width: '20%', display: 'flex', flexDirection: 'column', justifyContent: 'space-around', height: 'auto', padding: '60px' }}>
+            <ThemeProvider theme="dark" size="xlarge">
+              <div>
+                <Button fullWidth>Click me</Button>
+                <Button withRoundedCorners fullWidth textPosition="left">Click me</Button>
+                <Button withRoundedCorners fullWidth textPosition="center">Click me</Button>
+                <Button withRoundedCorners fullWidth textPosition="right">Click me</Button>
+              </div>
+            </ThemeProvider>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,37 +1,235 @@
 import React, {Component, PropTypes} from 'react';
+import styled, { css } from 'styled-components'
+import FontAwesome from 'react-fontawesome';
+
 import ThemedComponent from 'themedComponent';
-import styled from 'styled-components';
 
-const Button = styled.button`
-  /* Adapt the colors based on primary prop */
-  background: ${props => props.theme === 'light' ? '#fff' : '#337ab7'};
-  color: ${props => props.theme === 'light' ? '#333' : '#fff'};
+const xsmall = css`
+  ${props => props.size === 'xsmall' && 'padding: 1px 5px; font-size: 12px'};
+  ${props => (props.size === 'xsmall' && props.withRoundedCorners) && 'border-radius: 3px'};
+`;
 
-  line-height: 1.5;
+const small = css`
+  ${props => props.size === 'small' && 'padding: 5px 10px; font-size: 12px'};
+  ${props => (props.size === 'small' && props.withRoundedCorners) && 'border-radius: 3px'};
+`;
 
-  /*padding for all sizes*/
-  ${props => props.size === 'xsmall' && 'padding: 1px 5px;'
-    || props.size === 'small' && 'padding: 5px 10px;'
-    || props.size === 'medium' && 'padding: 6px 12px;'
-    || props.size === 'large' && 'padding: 10px 16px;'
-    || props.size === 'xlarge' && 'padding: 14px 20px;'
-  }
+const medium = css`
+  ${props => props.size === 'medium' && 'padding: 6px 12px; font-size: 14px'};
+  ${props => (props.size === 'medium' && props.withRoundedCorners) && 'border-radius: 4px'};
+`;
 
-  /*border-radius for all sizes*/
-  ${props => props.size === 'xsmall' && 'border-radius: 3px'
-    || props.size === 'small' && 'border-radius: 3px;'
-    || props.size === 'medium' && 'border-radius: 4px;'
-    || props.size === 'large' && 'border-radius: 6px;'
-    || props.size === 'xlarge' && 'border-radius: 8px;'
-  }
+const large = css`
+  ${props => props.size === 'large' && 'padding: 10px 16px; font-size: 18px'};
+  ${props => (props.size === 'large' && props.withRoundedCorners) && 'border-radius: 6px'};
+`;
 
-  /*font-size for all sizes*/
-  ${props => props.size === 'xsmall' && 'font-size: 12px;'
-    || props.size === 'small' && 'font-size: 12px;'
-    || props.size === 'medium' && 'font-size: 14px;'
-    || props.size === 'large' && 'font-size: 18px;'
-    || props.size === 'xlarge' && 'font-size: 18px;'
+const xlarge = css`
+  ${props => props.size === 'xlarge' && 'padding: 14px 20px; font-size: 18px'};
+  ${props => (props.size === 'xlarge' && props.withRoundedCorners) && 'border-radius: 8px'};
+`;
+
+const disabledStyle = css`
+  /* Disabled style */
+  ${props => props.disabled && 'border-color: #d3d6db; color: black; opacity: 0.5; pointer-events: none; cursor: not-allowed !important'};
+`;
+
+const textPosition = css`
+  /* Text Position*/
+  ${props => props.textPosition === 'center' && 'justify-content: center;'
+    || props.textPosition === 'left' && 'justify-content: flex-start;'
+    || props.textPosition === 'right' && 'justify-content: flex-end;'
   }
 `;
+
+const iconPosition = css`
+  /* Icon Position*/
+  ${props => props.iconPosition === 'before' && 'flex-direction: row;'
+    || props.iconPosition === 'after' && 'flex-direction: row-reverse;'
+    || props.iconPosition === 'up' && 'flex-direction: column;'
+    || props.iconPosition === 'down' && 'flex-direction: column-reverse;'
+  }
+`;
+
+const DefaultButton = styled.button`
+  /* Adapt the colors based on primary prop */
+  display: ${props => props.hidden ? 'none' : 'flex' };
+  justify-content: center;
+  align-items: center;
+  line-height: 1.5;
+  border-style: solid;
+  margin: 10px;
+
+  /* Full width */
+  width: ${props => props.fullWidth ? '100%' : 'auto'};
+
+  border-color: ${props => props.theme === 'light' ? '#fff' : '#0A7AB9'};
+  background: ${props => props.theme === 'light' ? '#fff' : '#0A7AB9'};
+  color: ${props => props.theme === 'light' ? '#333' : 'whitesmoke'};
+
+  /* Disabled style */
+  ${ disabledStyle }
+
+  /* Sizes style */
+  ${ xsmall }
+  ${ small }
+  ${ medium }
+  ${ large }
+  ${ xlarge }
+
+  /* Icon Position style */
+  ${ iconPosition}
+
+  /* Text Position style */
+  ${ textPosition }
+
+  &:hover {
+    opacity: 0.8;
+  }
+
+  &:active {
+    background-image: none;
+    outline: 0;
+    box-shadow: none;
+  }
+
+  &:focus {
+    background-image: none;
+    outline: 0;
+    box-shadow: none;
+  }
+`;
+
+const LinkButton = styled(DefaultButton)`
+  border: none;
+  border-bottom-width: 2px;
+  border-bottom-style: solid;
+  border-radius: 0;
+  background: transparent;
+  color: #337ab7;
+`;
+
+const TextButton = styled(DefaultButton)`
+  border: none;
+  border-radius: 0;
+  background: transparent;
+  color: #337ab7;
+`;
+
+const Icon = styled(FontAwesome)`
+  padding: 5px;
+`;
+
+class Button extends Component {
+  static propTypes = {
+    /**
+     * Width of button container
+     */
+    width: PropTypes.string,
+    /**
+     * Width of button container
+     */
+    height: PropTypes.string,
+    /**
+     * True if the button is disabled
+     */
+    isDisabled: PropTypes.bool,
+    /**
+     * Custom text for the button
+     */
+    text: PropTypes.string,
+    /**
+     * Choose the alignment of the given text
+     */
+    textPosition: PropTypes.oneOf(['left', 'center', 'right']),
+    /**
+     * onClick handler
+     */
+    onClick: PropTypes.func,
+    /**
+     * If true the button is invisible
+     */
+    hidden: PropTypes.bool,
+    /**
+     * Choose if the button will be displayed as a link or as a button
+     */
+    display: PropTypes.oneOf(['default', 'link', 'text']),
+    /**
+      * App renders any type of react component inside it
+      */
+    children: PropTypes.any,
+    /**
+      * Additional classNames
+      */
+    className: PropTypes.string,
+    /**
+      * Additional classNames
+      */
+    iconClassName: PropTypes.string,
+    /**
+     * Set button full width
+     */
+    fullWidth: PropTypes.bool,
+    /**
+     * Choose if the button will be displayed as a link or as a button
+     */
+    color: PropTypes.oneOf(['default', 'grey', 'red', 'black', 'blue', 'popup']),
+    /**
+     * Choose the icon name
+     */
+    icon: PropTypes.string,
+    /**
+     * Choose the icon library
+     */
+    iconLibrary: PropTypes.string,
+    /**
+     * Choose the icon position before text or after text
+     */
+    iconPosition: PropTypes.oneOf(['before', 'after', 'up', 'down']),
+    /**
+     * The tooltip title if button is icon button
+     */
+    title: PropTypes.string,
+    /**
+     * Set true if we want border Radius
+     */
+    withRoundedCorners: PropTypes.bool
+  }
+
+  static defaultProps = {
+    display: 'default',
+    isDisabled: false,
+    textPosition: 'center',
+    iconLibrary: 'fa',
+    iconPosition: 'before',
+    color: 'default',
+    theme: 'dark',
+    size: 'medium',
+    withRoundedCorners: false
+  }
+
+  renderIcon() {
+    let icon = null;
+    // Display an icon if it is declared in the props
+    if (this.props.icon) {
+      icon = (
+        <Icon name={this.props.icon}/>
+      );
+    }
+
+    return icon;
+  }
+
+  render() {
+    switch (this.props.display) {
+      case 'text':
+        return <TextButton {...this.props}>{this.renderIcon()}{this.props.children}</TextButton>;
+      case 'link':
+        return <LinkButton {...this.props}>{this.renderIcon()}{this.props.children}</LinkButton>;
+      default:
+        return <DefaultButton {...this.props}>{this.renderIcon()}{this.props.children}</DefaultButton>;
+    }
+  }
+}
 
 export default ThemedComponent(Button);
